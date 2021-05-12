2155d048098e53d9c1d5aa631f30ecf122a83e2a
Tim <timur555@yandex.ru>
2021-05-12 23:30:25 +0300

    JPP1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,129 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    Util util = new Util();
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE if not exists users(" +
+                    "id integer primary key auto_increment, " +
+                    "name varchar(45) not null, " +
+                    "lastname varchar(45) not null, " +
+                    "age tinyint not null );");
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.getClose();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = util.getConnection()) {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE IF EXISTS users;");
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.getClose();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        long id = 1;
+        String selectId = "SELECT * FROM users ORDER BY id DESC LIMIT 1;";
+        try (Connection connection = util.getConnection()) {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(selectId);
+            while (result.next()) {
+                id = result.getLong("id") + 1;
+            }
+            User user = new User();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(id, name, lastname, age)  values (?,?,?,?);");
+            preparedStatement.setLong(1, id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setByte(4, age);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            statement.close();
+            result.close();
+            preparedStatement.close();
+            System.out.println("User " + name + " add data base");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.getClose();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = util.getConnection()) {
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DELETE FROM users WHERE  id =" + id + ";");
+            connection.commit();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            util.getClose();
+        }
     }
 
     public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        User user;
+        try (Connection connection = util.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
+            while (resultSet.next()) {
+                user = new User(resultSet.getString("name"),
+                        resultSet.getString("lastname"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                userList.add(user);
+            }
+            statement.close();
+            resultSet.close();
+            return userList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            util.getClose();
+        }
         return null;
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = util.getConnection()){
+            connection.setAutoCommit(false);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("TRUNCATE users");
+            connection.commit();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            util.getClose();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,37 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testt?useSSL=false&serverTimezone=UTC";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public Util() {
+    }
+
+    Connection connection;
+
+    public Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+            return connection;
+        } catch (ClassNotFoundException| SQLException e) {
+            System.out.println("DB connection error");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void getClose() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
