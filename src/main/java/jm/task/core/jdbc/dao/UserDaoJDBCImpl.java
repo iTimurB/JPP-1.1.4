81be4c04081040f5aeb6ff04e4f2d4fc0c113f7a
Tim <timur555@yandex.ru>
2021-05-17 21:58:19 +0300

    JPP1.1.4_rev2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Util util = new Util();
+    private final Util util = new Util();
 
     public UserDaoJDBCImpl() {
     }
